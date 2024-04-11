766648ea005e4f39552d0abc4081262e002a9102
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-11 10:26:23 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.17 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,51 +1,20 @@
 import java.util.*;
 class Solution {
-    public static int solution(int[] array) {
-        int answer = 0;
-        int count1 = 0;
-        int count2 = 0;
-        int mostFrequent = 0;
+    public int solution(int[] array) { 
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int maxFrequency = 0;
+        int answer = -1;
 
-        Arrays.sort(array);
-
-        for (int i = 0; i < array.length; i++) {
-            count1 = 1;
-
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    count1++;
-                }
-            }
-
-            if (count1 > count2) {
-                count2 = count1;
-                mostFrequent = array[i];
-            } else if (count1 == count2) {
-                mostFrequent = -1;
+        for (int num : array) {
+            int frequency = frequencyMap.getOrDefault(num, 0) + 1;
+            frequencyMap.put(num, frequency);
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                answer = num;
+            } else if (frequency == maxFrequency) {
+                answer = -1;
             }
         }
-
-        return mostFrequent;
+        return answer;
     }
 }
-        
-        
-        
-    
-
-
-        
-        
-        
-    
-
-
-        
-        
-        
-    
-
-        
-        
-        
-    
