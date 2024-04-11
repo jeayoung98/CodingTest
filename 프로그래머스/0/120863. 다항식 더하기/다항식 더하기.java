8990811b4f42c160a99d383dc5c43176805c25ff
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-11 11:10:56 +0900

    [level 0] Title: 다항식 더하기, Time: 10.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,42 +1,30 @@
 import java.util.*;
-
 class Solution {
     public String solution(String str) {
         String answer = "";
-        str = str.replaceAll(" ", "");
-        int coe = 0; // 계수
-        int con = 0; // 상수
+        int xNum = 0;
+        int num = 0;
+        String str1 = str.replaceAll("\\s","");
+        String[] strArr = str1.split("");
+        int[] x = new int[strArr.length]; // 상수
 
-        // '+' 기호를 기준으로 문자열 분할
-        String[] parts = str.split("\\+");
-        for (String part : parts) {
-            if (part.contains("x")) { // x가 있을때
-                int xIndex = part.indexOf("x");
-                String coeStr = part.substring(0, xIndex);
-                if (coeStr.isEmpty()) {
-                    // 계수가 없으면 1로 처리
-                    coe += 1;
-                } else {
-                    coe += Integer.parseInt(coeStr);
+
+        System.out.println(Arrays.toString(strArr));
+        for (int i = 0; i < strArr.length; i++) {
+            if(strArr[i].equals("x")){
+                if(!strArr[i-1].equals("+")){
+                    xNum += Integer.parseInt(strArr[i-1]);
+                } if(strArr[i-1].equals("+")) xNum +=1;
+            }else {
+                if (!strArr[i].equals("x") && !strArr[i].equals("+") && !strArr[i+1].equals("x")) {
+                    x[i] = Integer.parseInt(strArr[i]);
                 }
-            } else { // 상수
-                con += Integer.parseInt(part);
             }
         }
-        
-        if(coe == 1){
-            answer = "x";
-        }else answer = coe + "x";
-
-        
-        if (con != 0) {
-            if (coe == 0) {
-                answer = String.valueOf(con);
-            } else {
-                answer += " + " + con;
-            }
+        for (int i = 0; i < x.length; i++) { // 상수 합
+            num+=x[i];
         }
-
+        answer = xNum + "x" +"+"+ num;
         return answer;
     }
 }
