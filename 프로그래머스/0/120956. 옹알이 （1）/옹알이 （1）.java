28a975ff0abd5c9d4f07a31321f37d03a0c43eb4
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-15 01:49:07 +0900

    [level 0] Title: 옹알이 (1), Time: 0.23 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] dic =  {"aya", "ye", "woo", "ma"};
+        for (int i = 0; i < babbling.length; i++) {
+            for (String str1 : dic) {
+                if (babbling[i].contains(str1)) {
+                    babbling[i] = babbling[i].replace(str1, "1");
+                }
+            }
+        }
+        for(int i = 0; i < babbling.length;i++){
+            if(isNum(babbling[i])){
+                answer+=1;
+            }
+        }
+        return answer;
+    }
+    
+    public static boolean isNum(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
