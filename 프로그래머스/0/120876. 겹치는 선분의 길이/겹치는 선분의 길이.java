8c0902b73f7e3907304cd79ad1d821b1b8f99cc5
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-15 02:39:07 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        for (int i = -100; i <= 100; i++) {
+            int count = 0;
+            if (lines[0][0] <= i && i < lines[0][1]) {
+                count++;
+            }
+            if (lines[1][0] <= i && i < lines[1][1]) {
+                count++;
+            }
+            if (lines[2][0] <= i && i < lines[2][1]) {
+                count++;
+            }
+            if (count > 1) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
