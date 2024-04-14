e29657297c1ac9de4911915987fbbf6ddd8c7f99
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-15 01:10:32 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        double[][] inclination = new double[3][2];
+        int x = 0;
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (j != i) {
+                    for (int k = 1; k <= 3; k++) {
+                        if (k != i && k != j) {
+                            inclination[x][0] = (double)(dots[0][1]-dots[i][1])/(dots[0][0]-dots[i][0]);
+                            inclination[x][1] = (double)(dots[j][1]-dots[k][1])/(dots[j][0]-dots[k][0]);
+                                x++;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0 ; i<inclination.length;i++){
+                if(inclination[i][0] == inclination[i][1]){
+                    answer = 1;
+                }
+        }
+        return answer;
+    }
+}
