ab193b7090f27e13bab4d9207b5b19d7d3a24629
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-21 01:46:57 +0900

    [level 1] Title: 시저 암호, Time: 27.52 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,11 +3,9 @@ class Solution {
         String answer = "";
         char[] ch = s.toCharArray();
         for(char c : ch){
-            if((c+"").equals(" ")){
-                answer += " ";
-            }else if((c + n > 90 && c <= 90) || (c + n > 122 && c <= 122)) answer += (char)(c + n -26);
+            if(c == ' ') answer += " ";
+            else if((c + n > 90 && c <= 90) || (c + n > 122 && c <= 122)) answer += (char)(c + n -26);
             else answer += (char)(c+n) +"";
-           
         }
         return answer;
     }
