ced53b42fe8a43a533ef5d3a2946aac6b2b67996
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-22 18:01:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,34 +1,34 @@
+import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String[][] wall = new String[n][n];
-        
-        for(int i = 0; i < n; i++){ // arr1 2진수 변환후 wall에 넣기
-            for(int j = 0; j < n; j++){
-                wall[i][j] = binary(arr1[i],n).charAt(j)+"";
-            }
+        String[] binary1 = new String[n];
+        String[] binary2 = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            binary1[i] = binary(arr1[i], n);
+            binary2[i] = binary(arr2[i], n);
         }
-        
-        for(int i = 0; i < n; i++){ // wall과 arr2 2진수 변환한거 1개라도 1이면 1
-            for(int j = 0; j < n; j++){
-                if(wall[i][j].equals("1") || binary(arr2[i],n).charAt(j) == '1') wall[i][j] = "1";
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (binary1[i].charAt(j) == '1' || binary2[i].charAt(j) == '1') {
+                    line.append("#");
+                } else {
+                    line.append(" ");
+                }
             }
-        }
-        
-        for(int i = 0; i < n; i++){ // wall의 1을 #으로 0을 공백으로
-            String str = "";
-            for(int j = 0; j < n; j++){
-                if(wall[i][j].equals("1")) str += "#";
-                else str += " ";
-            }answer[i] = str;
+            answer[i] = line.toString();
         }
         return answer;
     }
     
-    public String binary(int num,int n){
-        String answer = "";
-        answer += Integer.toBinaryString(num);
-        while(answer.length()<n) answer = "0" + answer;
-        return answer;
+    public String binary(int num, int n) {
+        StringBuilder result = new StringBuilder(Integer.toBinaryString(num));
+        while (result.length() < n) {
+            result.insert(0, '0');
+        }
+        return result.toString();
     }
 }
