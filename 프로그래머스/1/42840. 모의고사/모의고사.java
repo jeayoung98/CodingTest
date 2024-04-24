a2fab92aa505bf6180853fd3599477f8d5396588
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-24 18:47:06 +0900

    [level 1] Title: 모의고사, Time: 2.60 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] answers) {
+        int[] answer ={};
         int[] dropMath1 = {1, 2, 3, 4, 5};
         int[] dropMath2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] dropMath3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
@@ -18,7 +19,7 @@ class Solution {
         if(count[0] == maxScore) list.add(1);
         if(count[1] == maxScore) list.add(2);
         if(count[2] == maxScore) list.add(3);
-        
-        return list.stream().mapToInt(i -> i).toArray();
+        answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }
