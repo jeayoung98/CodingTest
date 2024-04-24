95838e41e9c7b07aeaccc8210bcf6ebc40d44d82
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-24 22:03:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        List<String> listCards1 = Arrays.asList(cards1);
+        List<String> listCards1 = Arrays.asList(cards1); // indexOf쓰려고
         List<String> listCards2 = Arrays.asList(cards2);
 
         int lastIndex1 = -1;
@@ -11,26 +11,24 @@ class Solution {
         int count1 = 0;
         int count2 = 0;
        
-        for (String str : goal) {
+        for (String str : goal) { // cards1
             if(listCards1.contains(str)){
                 int cardIndex = listCards1.indexOf(str);
-                if (cardIndex != lastIndex1 + 1) return "No";
+                if (cardIndex != lastIndex1 + 1) return "No"; // 건너뛰는지
                 lastIndex1 = cardIndex;
                 count1++;
             }
             
         }
 
-        for (String str : goal) {
+        for (String str : goal) { // cards2
             if(listCards2.contains(str)){
                 int cardIndex = listCards2.indexOf(str);
-                if (cardIndex != lastIndex2 + 1) return "No";
+                if (cardIndex != lastIndex2 + 1) return "No"; // 건너뛰는지
                 lastIndex2 = cardIndex;
                 count2++;
             }
         }
-    
-
         return answer;
     }
 }
