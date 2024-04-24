d92b3d5ae8f5d3ab04fd16c659a08c22a33bec9f
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-24 23:18:03 +0900

    [level 1] Title: 폰켓몬, Time: 3.89 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        if(set.size() < nums.length/2) return set.size();
+        return answer;
+    }
+}
