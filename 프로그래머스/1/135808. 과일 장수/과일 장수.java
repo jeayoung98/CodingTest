9fb0dc68d8d9d23284276db36d7552372c8e4055
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-04-30 17:38:18 +0900

    [level 1] Title: 과일 장수, Time: 0.39 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,11 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-
-//         int[] score1 = Arrays.stream(score)
-//                                 .filter(value -> value <= k)
-//                                 .toArray();
-        
         Arrays.sort(score);
         for (int i = score.length - m; i >= 0; i -= m) {
             try{
