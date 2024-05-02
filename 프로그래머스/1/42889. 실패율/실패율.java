de8fb2a9aae021a64aa0b1d7f26259e90a3a6ad5
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-02 17:15:31 +0900

    [level 1] Title: 실패율, Time: 0.64 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] stageCount = new int[N + 2];
+        
+        // 각 스테이지 클리어 못한 사람
+        for (int stage : stages) {
+            stageCount[stage]++;
+        }
+
+        // 각 스테이지에 도달한 사람
+        int[] reachCount = new int[N + 2];
+        reachCount[1] = stages.length;
+        for (int i = 2; i <= N; i++) {
+            reachCount[i] = reachCount[i - 1] - stageCount[i - 1];
+        }
+
+        // 스테이지 별 실패율
+        List<FailRate> failRates = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            double failRate = reachCount[i] == 0 ? 0 : (double) stageCount[i] / reachCount[i];
+            failRates.add(new FailRate(i, failRate));
+        }
+
+        // 실패율 기준 내림차순, 실패율이 같다면 스테이지 번호 작은 순
+        Collections.sort(failRates, new Comparator<FailRate>() {
+            @Override
+            public int compare(FailRate f1, FailRate f2) {
+                if (f1.failRate == f2.failRate) {
+                    return f1.stage - f2.stage;
+                }
+                return Double.compare(f2.failRate, f1.failRate);
+            }
+        });
+
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = failRates.get(i).stage;
+        }
+        
+        return answer;
+    }
+}
+
+class FailRate {
+    int stage;
+    double failRate;
+
+    public FailRate(int stage, double failRate) {
+        this.stage = stage;
+        this.failRate = failRate;
+    }
+}
