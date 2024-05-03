5ed8caefd9c45d03d18c8c9bbe38b5be5d7d6bc3
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-03 16:44:46 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.07 ms
+메모리: 76 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 22:03:23
+2024년 05월 03일 16:44:32
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,32 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        List<String> listCards1 = Arrays.asList(cards1); // indexOf쓰려고
-        List<String> listCards2 = Arrays.asList(cards2);
-
-        int lastIndex1 = -1;
-        int lastIndex2 = -1;
-        int count1 = 0;
-        int count2 = 0;
-       
-        for (String str : goal) { // cards1
-            if(listCards1.contains(str)){
-                int cardIndex = listCards1.indexOf(str);
-                if (cardIndex != lastIndex1 + 1) return "No"; // 건너뛰는지
-                lastIndex1 = cardIndex;
-                count1++;
-            }
-            
-        }
-
-        for (String str : goal) { // cards2
-            if(listCards2.contains(str)){
-                int cardIndex = listCards2.indexOf(str);
-                if (cardIndex != lastIndex2 + 1) return "No"; // 건너뛰는지
-                lastIndex2 = cardIndex;
-                count2++;
-            }
+        card(goal,cards1);
+        card(goal,cards2);
+        if (card(goal, cards1).equals("No") || card(goal, cards2).equals("No")) {
+            answer = "No";
         }
         return answer;
+    }
+    public String card(String[] goal,String[] list){
+        List<String> listCards = Arrays.asList(list);
+        int lastIndex = -1;
+        int count = 0;
+        for (String str : goal) { 
+            if(listCards.contains(str)){
+                int cardIndex = listCards.indexOf(str);
+                if (cardIndex != lastIndex + 1) return "No";
+                lastIndex = cardIndex;
+                count++;
+            }
+        }return "Yes";
     }
 }
