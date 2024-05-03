5bd1cf7988cff78ba7c1a777f1f3f1b3ae5cdf6a
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-03 17:41:10 +0900

    [level 1] Title: 옹알이 (2), Time: 0.13 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] babbling) {
+        int count = 0;
+        String[] bab = {"aya", "ye", "woo", "ma"};
+        
+        for (String word : babbling) {
+            if (isValidWord(word,bab)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isValidWord(String word,String[] bab) {
+        int index = 0;
+        String last = "";
+        while (index < word.length()) {
+            boolean found = false;
+            for (String pattern : bab) {
+                if (word.startsWith(pattern, index)) {
+                    // 연속된 같은 패턴 검사
+                    if (pattern.equals(last)) {
+                        return false;
+                    }
+                    // 패턴을 찾았다면 index를 업데이트
+                    index += pattern.length();
+                    last = pattern;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
