f9d05b53862382b903d76c9aa638b6fa4b8dd437
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-03 16:52:18 +0900

    [level 1] Title: 2016년, Time: 1.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,13 +1,6 @@
-public class Solution {
+import java.time.*;
+class Solution {
     public String solution(int a, int b) {
-        String[] Week = {"FRI", "SAT","SUN", "MON", "TUE", "WED", "THU"};
-        int[] Month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};  
-        int total = b;
-        for (int i = 0; i < a - 1; i++) {
-            total += Month[i];
-        }
-        int day = (total - 1) % 7;
-
-        return Week[day];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
     }
 }
