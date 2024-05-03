fb150825963e9b35ce048c450fac7c84ed1ee8f8
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-04 00:12:14 +0900

    [level 1] Title: 달리기 경주, Time: 0.16 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        // 말, 인덱스
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+
+        List<String> horseList = new ArrayList<>(Arrays.asList(players));
+
+        for (String horse : callings) {
+            Integer index = map.get(horse);
+            String frontHorse = horseList.get(index - 1);
+
+            // 말의 위치를 서로 바꿈
+            horseList.set(index - 1, horse);
+            horseList.set(index, frontHorse);
+
+            // 인덱스 맵 업데이트
+            map.put(horse, index - 1);
+            map.put(frontHorse, index);
+        }
+
+        return horseList.toArray(new String[0]);
+    }
+}
