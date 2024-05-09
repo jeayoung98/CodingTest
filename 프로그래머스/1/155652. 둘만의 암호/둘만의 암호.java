f3c3507c0336a745332b33eaf39c3f2af4b8b323
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-10 03:29:28 +0900

    [level 1] Title: 둘만의 암호, Time: 9.49 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,25 +1,20 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        StringBuilder result = new StringBuilder();
-        List<Character> list = new ArrayList<>();
-
-        // skip에 포함되지 않은 문자 추가
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            if (!skip.contains(String.valueOf(ch))) {
-                list.add(ch);
-            }
-        }
+        // skip에 있는 문자 제거
+        List<Character> list = "abcdefghijklmnopqrstuvwxyz".chars()
+                .mapToObj(c -> (char) c) // int를 char로 변환
+                .filter(c -> !skip.contains(String.valueOf(c)))
+                .collect(Collectors.toList());
+        list.stream().forEach(a -> System.out.print(a+" "));
 
         // 주어진 문자열 s의 각 문자에 대하여 변환 수행
-        for (char ch : s.toCharArray()) {
-            int currentIdx = list.indexOf(ch);
-            char c = list.get((currentIdx + index) % list.size());
-            result.append(c);
-        }
-        answer = result.toString();
-        return answer;
+        return s.chars()
+                .mapToObj(c -> (char) c)
+                .map(c -> list.get((list.indexOf(c) + index) % list.size()))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
