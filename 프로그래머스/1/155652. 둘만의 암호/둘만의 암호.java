292a24e3f66fafbbb674e54b10734c8494dfae50
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-10 03:30:09 +0900

    [level 1] Title: 둘만의 암호, Time: 9.89 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -10,7 +10,7 @@ public class Solution {
                 .collect(Collectors.toList());
         list.stream().forEach(a -> System.out.print(a+" "));
 
-        // 주어진 문자열 s의 각 문자에 대하여 변환 수행
+        // 인덱스만큼 움직이기
         return s.chars()
                 .mapToObj(c -> (char) c)
                 .map(c -> list.get((list.indexOf(c) + index) % list.size()))
