15660d819d0470d29c66d4407a876bdd9c891d01
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-15 15:34:29 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.20 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i : ingredient){
+            stack.push(i);
+            if(stack.size() >= 4){
+                boolean bread1 = stack.elementAt(stack.size()-1) == 1;
+                boolean meet = stack.elementAt(stack.size()-2) == 3;
+                boolean vegetable = stack.elementAt(stack.size()-3) == 2;
+                boolean bread2 = stack.elementAt(stack.size()-4) == 1;
+                if(bread1 && meet && vegetable && bread2) {
+                    answer++;
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                }
+            }
+            
+        }
+        return answer;
+    }
+}
