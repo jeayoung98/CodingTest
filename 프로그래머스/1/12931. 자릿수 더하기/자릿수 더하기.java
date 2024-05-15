27983b2e2e75de875ab12056ba259ce6669ac79c
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-15 14:44:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.91 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,12 +1,10 @@
+import java.util.stream.*;
 import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        while(n > 0){
-            answer += n % 10;
-            n /= 10;
-        }
-        return answer;
+        return Arrays.stream(String.valueOf(n).split(""))
+            .map(Integer::parseInt)
+            .reduce(0,(a,b)->a+b);
     }
 }
