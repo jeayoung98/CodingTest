bd7caafe2a9d1dbb7b3f537fbf32a2e454e3ee55
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-17 22:03:30 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.50 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        StringBuilder answer = new StringBuilder();
+        int left = 10; // * 위치 12
+        int right = 12; // # 위치 12
+        
+        for (int number : numbers) {
+            if (number == 0) number = 11; // 0 위치 11
+            
+            if (number % 3 == 1) { // 1, 4, 7
+                answer.append("L");
+                left = number;
+            } else if (number % 3 == 0) { // 3, 6, 9
+                answer.append("R");
+                right = number;
+            } else { // 2, 5, 8, 0
+                answer.append(middle(number, left, right, hand));
+                if (answer.charAt(answer.length() - 1) == 'L') {
+                    left = number;
+                } else {
+                    right = number;
+                }
+            }
+        }
+        
+        return answer.toString();
+    }
+
+    public String middle(int num, int left, int right, String hand) {
+        int leftDist = getDistance(left, num);
+        int rightDist = getDistance(right, num);
+
+        if (leftDist < rightDist) {
+            return "L";
+        } else if (rightDist < leftDist) {
+            return "R";
+        } else {
+            return hand.equals("right") ? "R" : "L";
+        }
+    }
+    
+    // 맨해튼 거리공식, 좌표평면 대입 
+    public int getDistance(int a, int b) {
+        int aRow = (a - 1) / 3;
+        int aCol = (a - 1) % 3;
+        int bRow = (b - 1) / 3;
+        int bCol = (b - 1) % 3;
+        return Math.abs(aRow - bRow) + Math.abs(aCol - bCol);
+    }
+}
