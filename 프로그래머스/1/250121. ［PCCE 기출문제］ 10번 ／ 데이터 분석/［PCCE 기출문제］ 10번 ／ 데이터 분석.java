fd97b2bf852d63d2b226602909cd64b50eb919a0
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-18 00:24:44 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 5.64 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -12,14 +12,10 @@ class Solution {
     }
 
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        // 정렬 Comparator 생성
-        Comparator<int[]> sortArr = (a, b) -> a[map.get(sort_by)] - b[map.get(sort_by)];
-        
-        // 정렬 수행
-        Arrays.sort(data, sortArr);
-        
-        // 필터링 수행하여 조건에 맞는 행만 선택
         return Arrays.stream(data)
+            // 정렬
+            .sorted(Comparator.comparingInt(a -> a[map.get(sort_by)]))
+            // 조건에 맞는 행
             .filter(a -> a[map.get(ext)] < val_ext)
             .toArray(int[][]::new);
     }
