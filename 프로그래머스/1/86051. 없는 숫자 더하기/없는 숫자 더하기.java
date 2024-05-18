0b05bf44b56027f0d3e7dd12ed223956084b0a14
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-19 01:09:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.68 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,9 +1,9 @@
+import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        for(int i = 0 ; i < numbers.length;i++){
-            answer -= numbers[i];
-        }
-        return answer;
+        int sum = Arrays.stream(numbers)
+            .sum();
+        return 45 - sum;
     }
 }
