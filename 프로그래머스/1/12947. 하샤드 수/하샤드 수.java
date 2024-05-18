bd452a3033b9cb415a3fc43cd15482a21ffe41be
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-19 00:06:50 +0900

    [level 1] Title: 하샤드 수, Time: 1.65 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean solution(int x) {
-        int sum = 0;
-		int a = x;
-      
-		while (a >= 1) {
-			sum += a % 10;
-			a /= 10;
-		}
-        return x % sum == 0 ? true:false;
+        int sum = String.valueOf(x)
+                        .chars()
+                        .map(Character::getNumericValue)
+                        .sum();
+        return x % sum == 0;
     }
 }
