a013ac3e3f782a03d58cacf18feda34dd105238e
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-19 18:41:25 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.80 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,9 +1,8 @@
+import java.util.stream.*;
 class Solution {
     public String solution(String phone_number) {
-        char[] chars = phone_number.toCharArray();
-        for (int i = 0; i < chars.length - 4; i++) {
-            chars[i] = '*';
-        }
-        return new String(chars);
+        return  IntStream.range(0,phone_number.length() - 4)
+                        .mapToObj(i -> "*")
+                        .collect(Collectors.joining()) + phone_number.substring(phone_number.length() - 4);
     }
 }
