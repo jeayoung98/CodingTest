939ab52a7639e4b12e099e9f669af4765e4d1ff6
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-19 20:11:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(long num) {
         int answer = 0;
-        while (answer <= 500) {
+        while (answer < 500) {
             if (num == 1) break;
             num = (num % 2 == 0) ? num / 2 : num * 3 + 1;
             answer++;
         }
-        return (answer >= 500) ? -1 : answer;
+        return (answer == 500) ? -1 : answer;
     }
 }
