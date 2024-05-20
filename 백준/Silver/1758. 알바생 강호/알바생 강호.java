a9e3be282cf361b596116a1b6f838361b22d84e7
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-20 18:51:00 +0900

    [Silver IV] Title: 알바생 강호, Time: 964 ms, Memory: 104020 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Integer[] tipArr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            int tip = sc.nextInt();
+            tipArr[i] = tip;
+        }
+        Arrays.sort(tipArr, (a, b) -> b - a);
+
+        long sum = IntStream.range(0, n)
+                        .mapToLong(i -> tipArr[i]-i)
+                        .filter(tip -> tip>=0)
+                        .sum();
+        System.out.println(sum);
+    }
+}
