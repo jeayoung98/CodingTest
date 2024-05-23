faf732140ab542de6b06afb129521f3a05e80796
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-23 23:06:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.76 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,9 @@
+import java.util.stream.*;
+import java.util.*;
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        int num = n/2;
-        for(int i = 1; i <= num ; i++) answer += "수박";
-        if(n % 2 != 0) answer += "수";
-        return answer;
+        return IntStream.rangeClosed(1,n)
+            .mapToObj(i -> i % 2 == 0?"박":"수")
+            .collect(Collectors.joining());
     }
 }
