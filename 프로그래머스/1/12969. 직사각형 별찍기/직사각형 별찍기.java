1024d86b83d80c5bb20e0a09910dad6073402ab8
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-05-25 23:39:41 +0900

    [level 1] Title: 직사각형 별찍기, Time: 145.19 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.stream.*;
+import java.util.*;
 
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        String star = "";
-        for(int i = 0; i < a;i++){
-            star += "*";
-        }
-        for(int i = 0; i < b; i++){
-            System.out.println(star);
-        }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] i = br.readLine().split(" ");
+        String stars = IntStream.range(0,Integer.parseInt(i[0]))
+                                .mapToObj(num -> "*")
+                                .collect(Collectors.joining());
+        IntStream.range(0,Integer.parseInt(i[1]))
+                .forEach(m -> System.out.println(stars));
+        
     }
 }
