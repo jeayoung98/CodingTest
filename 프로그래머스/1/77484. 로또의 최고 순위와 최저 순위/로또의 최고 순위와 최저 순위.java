050b5d8ff54ef5faf89d2300682193fd12d67899
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-06-08 22:41:32 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,21 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int count = 0;
-        int zero = 0;
+        int[] answer = new int[]{7, 7};
+        // 당첨 번호를 저장할 HashSet 생성
         Set<Integer> set = new HashSet<>();
         for (int num : win_nums) {
             set.add(num);
         }
-        
-        for(int i : lottos){
-            if(set.contains(i)) count++;
-            if(i == 0) zero++;
+
+        // 당첨 번호와 0의 개수
+        for (int i : lottos) {
+            if (set.contains(i)) {
+                answer[0]--; // 최고 순위 증가
+                answer[1]--; // 최저 순위 증가
+            }
+            if (i == 0) {
+                answer[0]--; // 0의 개수만큼 최고 순위 감소
+            }
         }
+
+        // 순위가 7인 경우 6
+        answer[1] = answer[1] == 7 ? 6 : answer[1];
+        answer[0] = answer[0] == 7 ? 6 : answer[0];
         
-        answer[1] = 7 - count == 7 ? 6 : 7 - count;
-        answer[0] = 7 - count - zero == 7 ? 6 : 7 - count - zero;
         return answer;
     }
 }
