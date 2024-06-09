bb608e2214fbc9e1bb7225aceeabc6cc21b474c9
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-06-09 18:13:50 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,34 +1,39 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] clothes = new int[n];
-        Arrays.fill(clothes, 1);
-
-        for (int r : reserve)
-            clothes[r-1] ++;
-        for (int l : lost)
-            clothes[l-1] --;
-
-        for (int i = 0; i < n; i++) {
-            if (clothes[i] >= 1) {
-                answer++;
-            } else { 
-                // 왼쪽 여분
-                if (i >= 1 && clothes[i - 1] == 2) { 
-                    clothes[i - 1]--;
-                    clothes[i]++;
-                    answer++;
-                // 오른쪽 여분
-                } else if (i < n-1 && clothes[i + 1] == 2) { 
-                    clothes[i + 1]--;
-                    clothes[i]++;
-                    answer++;
+        Map<Integer,Integer> map = new HashMap<>();
+        // 모든 학생이 체육복 1개씩 가지고 있음
+        for(int i = 1; i<=n;i++){
+            map.put(i,1);
+        }
+        
+        // 누가 훔쳐감 ㅅㅂ
+        for(int i : lost){
+            map.put(i,map.get(i)-1);
+        }
+        
+        // 여분 체육복
+        for(int i : reserve){
+            map.put(i,map.get(i)+1);
+        }
+        
+        // 옆에 애 빌려줌
+        for(int i = 1; i <= n; i++){
+            if (map.get(i) > 1) {
+                if (i > 1 && map.get(i - 1) == 0) {
+                    map.put(i - 1, 1);
+                    map.put(i, 1);
+                } else if (i < n && map.get(i + 1) == 0) {
+                    map.put(i + 1, 1);
+                    map.put(i, 1);
                 }
             }
         }
-
+        for(int i = 1; i <= n; i++){
+            if(map.get(i) >= 1) answer++;
+        }
+        
         return answer;
     }
 }
