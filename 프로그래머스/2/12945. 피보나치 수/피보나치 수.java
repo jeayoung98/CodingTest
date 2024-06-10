95e5918aeedfbd26364a2d924cd1d06f0cb73715
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-06-10 14:40:25 +0900

    [level 2] Title: 피보나치 수, Time: 12.44 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,11 @@
+import java.util.stream.Stream;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int a = 1;
-        int b = 1;
-        for(int i = 2;i < n; i++){
-            answer = (a + b) % 1234567;
-            a = b % 1234567;
-            b = answer;
-        }
-        return answer;
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], (f[0] + f[1]) % 1234567})
+                     .limit(n)
+                     .map(f -> f[1])
+                     .reduce((a, b) -> b)
+                     .orElse(0);
     }
 }
