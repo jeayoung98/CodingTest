680ec34a9e7dde88a8431d3b46fce8e86bd2555b
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-06-16 18:22:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,7 +1,9 @@
 class Solution{
     public int solution(int n, int a, int b){
         int answer = 1;
+        // 대결할때까지 각각의 번호를 줄임
         while(true){
+            // 작은 수가 홀수이고, 차이가 1이여야 대결함
             if(Math.min(a,b) % 2 == 1 && Math.abs(a-b) == 1) break;
             a -= a/2;
             b -= b/2;
