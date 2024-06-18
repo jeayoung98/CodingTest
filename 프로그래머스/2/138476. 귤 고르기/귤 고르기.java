1dfe9cf925bf1ed602f08529d8de0dbbd43a7969
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-06-18 21:05:23 +0900

    [level 2] Title: 귤 고르기, Time: 152.45 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        Arrays.sort(tangerine);
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0)+1);
+        }
+        
+        int[] sortedArr = Arrays.stream(tangerine)
+                                .boxed()
+                                .sorted((a,b)-> map.get(b) - map.get(a))
+                                .mapToInt(i -> i)
+                                .toArray();
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < k; i++){
+            set.add(sortedArr[i]);
+        }
+        return set.size();
+    }
+}
