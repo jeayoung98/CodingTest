cc6c86dcf5c367d4999a7f6f63b65b4fd6b69197
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-07-29 00:37:59 +0900

    [level 2] Title: 피로도, Time: 55.35 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,47 +1,14 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
+    public int ans = 0;
     public int solution(int k, int[][] dungeons) {
-        int n = dungeons.length;
-        List<Integer> index = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            index.add(i);
-        }
-
-        int result = 0;
-        // 모든 던전 순서 순열을 생성
-        do {
-            int currentFatigue = k;
-            int count = 0;
-            for (int i : index) {
-                int[] dungeon = dungeons[i];
-                if (currentFatigue >= dungeon[0]) { // 최소 필요 피로도 검사
-                    currentFatigue -= dungeon[1]; // 피로도 소모
-                    count++;
-                } else {
-                    break;
-                }
-            }
-            result = Math.max(result, count);
-        } while (nextPermutation(index));
-
-        return result;
+        dfs(k,dungeons,0);
+        return ans;
     }
-    // 순열을 생성하는 함수
-    private boolean nextPermutation(List<Integer> list) {
-        int k = list.size() - 2;
-        while (k >= 0 && list.get(k) >= list.get(k + 1)) {
-            k--;
-        }
-        if (k == -1) return false;
-
-        int l = list.size() - 1;
-        while (list.get(k) >= list.get(l)) {
-            l--;
-        }
-
-        Collections.swap(list, k, l);
-        Collections.reverse(list.subList(k + 1, list.size()));
-        return true;
+    public void dfs(int k,int[][]d,int a){
+        ans = Math.max(a,ans);
+        for(int i =0;i<d.length;i++) if(k>=d[i][0]) dfs(k - d[i][1], Stream.concat(Arrays.stream(Arrays.copyOfRange(d, 0, i)), Arrays.stream(Arrays.copyOfRange(d, i + 1, d.length))).toArray(int[][]::new), a + 1);
     }
 }
