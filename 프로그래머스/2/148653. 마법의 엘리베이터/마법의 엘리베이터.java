0d20e3c4570b198da8efe734f174d90750e57256
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-09-12 11:27:02 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.25 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,22 @@
+class Solution {
+    private String str;
+    private int result;
+
+    public int solution(int storey) {
+        this.str = Integer.toString(storey);
+        this.result = Integer.MAX_VALUE;
+        int n = str.length();
+        dfs(n - 1, 0, 0);
+        return result;
+    }
+
+    private void dfs(int cursor, int offset, int score) {
+        if (cursor == -1) {
+            result = Math.min(result, score + offset);
+            return;
+        }
+        int num = Character.getNumericValue(str.charAt(cursor)) + offset;
+        dfs(cursor - 1, 0, score + num);
+        dfs(cursor - 1, 1, score + 10 - num);
+    }
+}
