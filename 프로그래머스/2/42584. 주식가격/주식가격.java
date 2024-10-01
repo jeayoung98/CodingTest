c7ba6bbb770273ad431c4fc599a28a343d444e6c
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-10-02 03:52:54 +0900

    [level 2] Title: 주식가격, Time: 30.20 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,12 +5,11 @@ class Solution {
         int[] answer = new int[prices.length];
         Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < prices.length; i++) {
-
-        while (!deque.isEmpty() && prices[i] < prices[deque.peek()]) {
-            int index = deque.pop();
-            answer[index] = i - index; 
-        }
-        deque.push(i); 
+            while (!deque.isEmpty() && prices[i] < prices[deque.peek()]) {
+                int index = deque.pop();
+                answer[index] = i - index; 
+            }
+            deque.push(i); 
         }
 
         while (!deque.isEmpty()) {
