446dd905876763493e5a85e661ecef1dc2e94f06
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-10-14 20:20:14 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 30.87 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,21 +1,57 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String[] solution(String[] files) {
         String[] answer = {};
-        String[] arr = new String[3];
-        Deque<Character> deque = new ArrayDeque<>();
-        for(int i = 0 ; i < files.length; i++){
-            for(int j = 0 ; j < files[i].length(); j++){
-                deque.add(files[i].charAt(j));
-            }
-            
+        List<String[]> list = new ArrayList<>();
+        
+        for(int i = 0; i < files.length; i++){
+            String str = files[i];
+            int j = 0;
+            String[] arr = new String[3];
+            arr[0] = head(str, j);
+            j += arr[0].length(); 
+            arr[1] = num(str, j);
+            j += arr[1].length(); 
+            arr[2] = tail(str, j); 
+            list.add(arr);
         }
-        System.out.println(deque);
-        return answer;
+        List<String> sortedList = list.stream()
+                            .sorted((a, b) -> {
+                                int result = a[0].toUpperCase().compareTo(b[0].toUpperCase());
+                                if (result != 0) {
+                                    return result;
+                                }
+                                return Integer.compare(Integer.parseInt(a[1]), Integer.parseInt(b[1]));
+                            }).map(arr -> String.join("", arr)).collect(Collectors.toList());
+        return sortedList.toArray(new String[0]);
     }
     
-    // public void head(){
-    //     while()
-    // }
+    public String head(String str, int j) {
+        StringBuilder s = new StringBuilder();
+        while(j < str.length() && !Character.isDigit(str.charAt(j))){
+            s.append(str.charAt(j));
+            j++;
+        }
+        return s.toString();
+    }
+    
+    public String num(String str, int j) {
+        StringBuilder s = new StringBuilder();
+        while(j < str.length() && Character.isDigit(str.charAt(j))){
+            s.append(str.charAt(j));
+            j++;
+        }
+        return s.toString();
+    }
+    
+    public String tail(String str, int j) {
+        StringBuilder s = new StringBuilder();
+        while(j < str.length()){
+            s.append(str.charAt(j));
+            j++;
+        }
+        return s.toString();
+    }
 }
