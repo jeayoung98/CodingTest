b5ef2f05e80786889a47d41e5916e260ff33461a
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-10-14 20:45:15 +0900

    [level 2] Title: 숫자 변환하기, Time: 83.10 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+    public static int solution(int x, int y, int n) {
+        if (x == y) return 0;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[y + 1];
+        queue.offer(x);
+        visited[x] = true;
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int current = queue.poll();
+                int[] results = {current + n, current * 2, current * 3};
+                for (int result : results) {
+                    if (result == y) {
+                        return count + 1; 
+                    }
+                    if (result <= y && !visited[result]) {
+                        queue.offer(result);
+                        visited[result] = true;
+                    }
+                }
+            }
+            count++;
+        }
+        return -1; 
+    }
+}
