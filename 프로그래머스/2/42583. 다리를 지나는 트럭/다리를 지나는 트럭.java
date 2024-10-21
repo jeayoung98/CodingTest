2665fe068d62a2785f136b12555c97b14d648cdb
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-10-21 13:40:34 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.64 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    int totalWeight = 0;
+    int answer = 0;
+    Deque<Integer> bridge = new ArrayDeque<>();
+    public int solution(int bridge_length, int weight, int[] truck_weights) { 
+        int time = 0;
+        int i = 0;
+
+        while (i < truck_weights.length) {
+            time++;
+
+            if (bridge.size() == bridge_length) {
+                totalWeight -= bridge.pollFirst();
+            }
+
+            if (totalWeight + truck_weights[i] <= weight && bridge.size() < bridge_length) {
+                bridge.add(truck_weights[i]);
+                totalWeight += truck_weights[i];
+                i++;
+            } else {
+                bridge.add(0);
+            }
+        }
+
+        answer = time + bridge_length;
+        return answer;
+    }
+}
