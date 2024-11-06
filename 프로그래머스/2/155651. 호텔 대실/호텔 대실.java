0ad3f129d0085ecb46e62623578f013185ae6d1b
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-11-06 14:08:29 +0900

    [level 2] Title: 호텔 대실, Time: 22.19 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        // 분으로 변경
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        // 시작 시간 기준 정렬
         Arrays.sort(book_time, (a, b) -> a[0].compareTo(b[0]));  
-
         for (String[] time : book_time) {
             int start = toMinutes(time[0]);
             int end = toMinutes(time[1]) + 10;
