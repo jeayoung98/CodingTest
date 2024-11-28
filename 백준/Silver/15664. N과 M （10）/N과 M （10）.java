4358b3911827638c661da0bcdf410a8db2ccb565
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-11-28 12:10:14 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] result;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        String[] s1 = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        arr = new int[N];
+        result = new int[M];
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(s1[i]);
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int last = -1;
+        for (int i = start; i < N; i++) {
+            if (last == arr[i]) continue;
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                last = arr[i];
+                dfs(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
