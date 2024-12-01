f2796364f83ae36658c5be22ae477bd5edb3f2ef
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-12-02 07:43:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -9,12 +9,12 @@ public class Main {
 
         int[] dp = new int[11];
         dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        for (int i = 1; i <= 10; i++) {
-            dp[i] = 0;
-            if (i - 1 >= 0) dp[i] += dp[i - 1];
-            if (i - 2 >= 0) dp[i] += dp[i - 2];
-            if (i - 3 >= 0) dp[i] += dp[i - 3];
+        for(int i = 4; i <= 10; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
 
         for (int i = 0; i < N; i++) {
