cd20676201a757ec4ef18b42a55e57a49ebdc8a8
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-12-03 07:41:12 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1412 ms, Memory: 97384 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -10,14 +10,14 @@ public class Main {
         int N = Integer.parseInt(a[0]);
         int M = Integer.parseInt(a[1]);
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        long[] sum = new long[N+1];
+        long[] sum = new long[N + 1];
         sum[0] = 0;
         for (int i = 1; i <= arr.length; i++) {
-            sum[i] = sum[i - 1] + arr[i-1];
+            sum[i] = sum[i - 1] + arr[i - 1];
         }
         for (int i = 0; i < M; i++) {
             int[] K = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            long result = sum[K[1]]-sum[K[0]-1];
+            long result = sum[K[1]] - sum[K[0] - 1];
             System.out.println(result);
         }
     }
