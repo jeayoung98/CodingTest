7790b1e5a5f373284e4c6ccbbdebcfc1bc2677af
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-12-16 19:43:53 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1080 ms, Memory: 194208 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -9,41 +9,16 @@ public class Main {
 
         br.readLine();
         int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(A);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
+        }
 
         br.readLine();
         int[] B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         StringBuilder sb = new StringBuilder();
-        Arrays.stream(B).forEach(a -> sb.append(findLast(A, a) - findFirst(A, a)).append(" "));
+        Arrays.stream(B).forEach(a -> sb.append(map.getOrDefault(a,0)).append(" "));
         System.out.println(sb.toString());
-    }
-    
-    public static int findFirst(int[] A, int x) {
-        int left = 0;
-        int right = A.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (A[mid] < x) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }
-    
-    public static int findLast(int[] A, int x) {
-        int left = 0;
-        int right = A.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (A[mid] <= x) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
     }
 }
