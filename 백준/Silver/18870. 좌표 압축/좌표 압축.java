8fba6cf4baa9e3eedfe03a5c5fa7714d1567eea4
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-12-17 15:10:14 +0900

    [Silver II] Title: 좌표 압축, Time: 1760 ms, Memory: 231404 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,34 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+
         String[] s = br.readLine().split(" ");
         int[] arr = new int[N];
-        int[] temp = new int[N];
-        for (int i = 0; i < s.length; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(s[i]);
-            temp[i] = Integer.parseInt(s[i]);
         }
 
-        Arrays.sort(temp);
-        int sequence = -1;
-        int last = Integer.MAX_VALUE;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < temp.length; i++) {
-            if (last == temp[i]) map.put(temp[i],sequence);
-            else map.put(temp[i],++sequence);
-            last = temp[i];
-        }
+        int[] array = Arrays.stream(arr).distinct().toArray();
+        Arrays.sort(array);
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(map.get(arr[i])).append(" ");
+        for (int i = 0; i < N; i++) {
+            int x = Arrays.binarySearch(array, arr[i]);
+            sb.append(x).append(" ");
         }
-        System.out.println(sb);
+
+        System.out.println(sb.toString().trim());
     }
 }
