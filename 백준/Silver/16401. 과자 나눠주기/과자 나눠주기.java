b5f27bc6dfe83164f8568e273fa4e56336fa66fb
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2024-12-25 19:38:27 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1220 ms, Memory: 144440 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        int[] snacks = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(snacks);
+        long result = getResult(snacks, N, M);
+
+        System.out.println(result);
+    }
+
+    private static long getResult(int[] snacks, int N, int M) {
+        int max = snacks[snacks.length - 1];
+
+        int left = 1;
+        int right = max;
+        int result = 0;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int total = 0;
+
+            for(int i = 0; i < M; i++) {
+                total += (snacks[i] / mid);
+                if(total >= N) break;
+
+            }
+
+            if(total >= N) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
