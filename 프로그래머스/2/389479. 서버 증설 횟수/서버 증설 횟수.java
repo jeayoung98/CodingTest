58169e0a713adce35046ecbf30c7813be5ac49a9
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2025-02-24 21:32:08 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.78 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    int M,K,answer,time;
+    Queue<Integer> queue = new PriorityQueue<>();
+    public int solution(int[] players, int m, int k) {
+        M = m;
+        K = k;
+        for(int i = 0; i < players.length; i++){
+            int currentPlayer = players[i];
+            endServer();
+            addServer(currentPlayer);
+            time++;
+        }
+        return answer;
+    }
+    
+    public void addServer(int currentPlayer){
+        if(currentPlayer >= (queue.size() + 1) * M){
+            while(true){
+                if(currentPlayer < (queue.size() + 1) * M){
+                    break;
+                } 
+                queue.add(time + K);
+                answer++;
+            }
+        }
+    }
+    
+    public void endServer(){
+        while(!queue.isEmpty() && queue.peek() <= time){
+            queue.poll();
+        }
+    }
+}
