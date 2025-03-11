f25cb68ec537420e568f3b89ae8669e11abdbec3
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-03-11 12:01:37 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
+    static int N, M;
     static int[] arr;
     static boolean[] visited;
-    static Set<List<Integer>> set;
-
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        N = array[0];
-        M = array[1];
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
         arr = new int[M];
         visited = new boolean[N + 1];
-        set = new HashSet<>();
         dfs(0);
+        System.out.println(sb.toString());
     }
 
     public static void dfs(int depth) {
         if (depth == M) {
-            List<Integer> a = new ArrayList<>();
             for (int i = 0; i < M; i++) {
-                a.add(arr[i]);
+                sb.append(arr[i]).append(" ");
             }
-            a.sort((x,y)->x-y);
-            if (!set.contains(a)) {
-                for (int i = 0; i < M; i++) {
-                    System.out.print(arr[i] + " ");
-                }
-                System.out.println();
-            }
-
-            set.add(a);
+            sb.append("\n");
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = depth == 0 ? 1 : arr[depth - 1]; i <= N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 arr[depth] = i;
