519829fabb50350796b252a0d636ee4cf346cedb
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-03-18 15:01:49 +0900

    [level 2] Title: 광물 캐기, Time: 1.17 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+    int picksCount = 0;
+    public int solution(int[] picks, String[] minerals) {
+        int answer = 0;
+        for(int i = 0; i < picks.length; i++){
+            picksCount += picks[i];
+        }
+        String[][] mineralArr = new String[minerals.length / 5 + 1][5];
+        
+        for(int i = 0; i < minerals.length; i++){
+            mineralArr[i / 5][i % 5] = minerals[i];
+        }
+        
+        Queue<int[]> queue = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        for(int i = 0; i < ((picksCount < minerals.length / 5 + 1) ? picksCount : minerals.length / 5 + 1); i++){
+            queue.add(setPoint(mineralArr[i], i));
+        }
+        
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentIndex = current[1];
+
+            int currentPick = -1;
+            for(int i = 0; i < 3; i++){
+                if(picks[i] > 0){
+                    currentPick = i;
+                    picks[i]--;
+                    break;
+                }
+            }
+            if(currentPick == -1){
+                break;
+            }
+
+            for(int i = 0; i < 5; i++){
+                String currentMineral = mineralArr[currentIndex][i];
+                if(currentMineral == null) break;
+
+                if(currentMineral.equals("diamond")){
+                    if(currentPick == 0) answer += 1;
+                    else if(currentPick == 1) answer += 5;
+                    else answer += 25;
+                }
+                else if(currentMineral.equals("iron")){
+                    if(currentPick == 0) answer += 1;
+                    else if(currentPick == 1) answer += 1;
+                    else answer += 5;
+                }
+                else {
+                    if(currentPick == 0) answer += 1;
+                    else if(currentPick == 1) answer += 1;
+                    else answer += 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int[] setPoint(String[] arr, int index){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == null) break;
+            
+            if(arr[i].equals("diamond")) sum += 25;
+            else if(arr[i].equals("iron")) sum += 5;
+            else sum += 1;
+        }
+        return new int[] {sum, index};
+    }
+}
