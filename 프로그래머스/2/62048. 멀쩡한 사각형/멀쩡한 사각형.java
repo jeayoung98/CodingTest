c4ac479a350c98359e36783e9bc2796ad253784f
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-03-22 23:20:50 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.08 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,7 +1,7 @@
 class Solution {
     public static long solution(int w, int h) {
         int gcd = gcd(w,h);
-        return (long)w * h - gcd * (w / gcd + h / gcd - 1);
+        return (long)w * h - (w + h - gcd);
     }
 
     public static int gcd(int a, int b) {
