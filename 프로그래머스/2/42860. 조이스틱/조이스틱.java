6abcf7b45ac115dbbb8d0b5568acee237f028d0a
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-03-27 14:19:50 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);
+        }
+        
+        int move = name.length() - 1;
+        for (int i = 0; i < name.length(); i++) {
+            int next = i + 1;
+            while (next < name.length() && name.charAt(next) == 'A') {
+                next++;
+            }
+            move = Math.min(move, i + name.length() - next + Math.min(i, name.length() - next));
+        }
+        answer += move;
+        return answer;
+    }
+}
