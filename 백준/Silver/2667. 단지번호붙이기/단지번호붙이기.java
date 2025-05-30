ba43ff76567f85ede2ad0f9162e50d7b7367c665
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-05-30 22:19:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,60 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    static int[][] apt;
     static boolean[][] visited;
+    static int[][] arr;
     static Queue<int[]> queue;
+    static int N, count = 0;
     static int[] dx = new int[]{0, 0, 1, -1};
     static int[] dy = new int[]{1, -1, 0, 0};
-    static int N;
+    static List<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        apt = new int[N][N];
+        arr = new int[N][N];
         visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split("");
-            for (int j = 0; j < s.length; j++) {
-                apt[i][j] = Integer.parseInt(s[j]);
-            }
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
-        int count = 0;
-        List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && apt[i][j] == 1) {
-                    list.add(bfs(i,j));
-                    count++;
-                }
+                if (arr[i][j] == 1 && !visited[i][j]) bfs(i,j);
             }
         }
         System.out.println(count);
-        list.sort((a,b)->a-b);
-        list.forEach(System.out::println);
+        Collections.sort(list);
+        for (int i : list) {
+            System.out.println(i);
+        }
     }
 
-    public static int bfs(int x, int y) {
+    public static void bfs(int x, int y) {
         queue = new LinkedList<>();
         queue.add(new int[]{x, y});
         visited[x][y] = true;
         int size = 1;
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int cx = current[0];
-            int cy = current[1];
+            int nx = current[0];
+            int ny = current[1];
             for (int i = 0; i < dx.length; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && apt[nx][ny] == 1) {
-                    queue.add(new int[]{nx, ny});
-                    visited[nx][ny] = true;
+                int cx = nx + dx[i];
+                int cy = ny + dy[i];
+                if (cx >= 0 && cx < N && cy >= 0 && cy < N && arr[cx][cy] == 1 && !visited[cx][cy]) {
+                    queue.add(new int[]{cx, cy});
+                    visited[cx][cy] = true;
                     size++;
                 }
             }
         }
-        return size;
+        list.add(size);
+        count++;
     }
 }
