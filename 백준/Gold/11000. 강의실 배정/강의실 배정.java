9c4e2e1eb8749ae115fdaad475bda581cbdde8da
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-05-31 22:36:32 +0900

    [Gold V] Title: 강의실 배정, Time: 892 ms, Memory: 128456 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -3,32 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        List<int[]> list = new ArrayList<>();
+        int[][] arr = new int[N][2];
         for (int i = 0; i < N; i++) {
-            list.add(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        list.sort((a,b)->{
-            if (a[0] != b[0]) {
-                return a[0] - b[0];
-            }
-            return a[1] - b[1];
+
+        Arrays.sort(arr,(a,b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
         });
 
         Queue<Integer> queue = new PriorityQueue<>();
 
-        for (int[] arr : list) {
-            if (!queue.isEmpty() && queue.peek() <= arr[0]) {
+        for (int[] time : arr) {
+            if (!queue.isEmpty() && queue.peek() <= time[0]) {
                 queue.poll();
             }
-            queue.add(arr[1]);
+            queue.add(time[1]);
         }
         System.out.println(queue.size());
-
     }
 }
