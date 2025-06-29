d70e97ac00b1b9d6ae0ed94da6337333eff96a1e
Jaeyoung <160308043+jeayoung98@users.noreply.github.com>
2025-06-29 17:13:12 +0900

    [level 2] Title: 예상 대진표, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,30 @@
-function solution(n, a, b) {
-  var answer = 0;
-  while (a !== b) {
-    a = Math.ceil(a / 2);
-    b = Math.ceil(b / 2);
-    answer++;
-  }
-  return answer;
+function solution(n,a,b){
+    var answer = 0;
+    
+    while(n >= 2){
+        if((a > n / 2 && b > n / 2)||(a <= n / 2 && b <= n / 2)){
+            n /= 2;
+            if(a > n){
+                a -= n;
+                b -= n;
+            }
+        }
+        else{
+            answer = powNum(n);
+            break;
+        } 
+    }
+    if(n === 1) answer = 1;
+    
+
+    return answer;
+}
+
+function powNum(n){
+    let result = 0;
+    while(n > 1){
+        n = n / 2;
+        result++;
+    }
+    return result;
 }
