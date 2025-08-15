c35f81c1042847b0b481e398a0e8f09e5f353750
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2025-08-15 19:17:21 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 3.35 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,45 +1,14 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        for(int i = 0; i < numbers.length;i++){
-            if(numbers[i] % 2 == 0) answer[i] = numbers[i] + 1;
-            else{
-                char[] c = Long.toBinaryString(numbers[i]).toCharArray();
-                List<Character> list = IntStream.range(0, c.length)
-                                        .mapToObj(a -> c[a])  
-                                        .collect(Collectors.toList());
-                if(list.size() == Long.bitCount(numbers[i])){
-                    answer[i] = nextNum(list.size()+1);
-                    System.out.println(list);
-                }else{
-                    int lastIndex = list.lastIndexOf('0');
-                    list.set(lastIndex, '1');
-                    int nextIndex = list.subList(lastIndex + 1, list.size()).indexOf('1') + lastIndex + 1;
-                    list.set(nextIndex, '0');
-                    answer[i] = Long.parseLong(list.stream()
-                                            .map(String::valueOf)
-                                            .collect(Collectors.joining()), 2);
-                }
+        for(int i = 0; i < numbers.length; i++){
+            long currentNum = numbers[i];
+            if((currentNum & 1L) == 0){
+                answer[i] = currentNum + 1L;
+            }else{
+                answer[i] = currentNum + 1 + ((currentNum ^ (currentNum + 1)) >>> 2);
             }
         }
         return answer;
     }
-    public long nextNum(int size){
-        List<Character> list = new ArrayList<>();
-        list.add('1');
-        list.add('0');
-        while(list.size() < size){
-            list.add('1');
-        }
-        long result = Long.parseLong(list.stream()
-                            .map(String::valueOf)
-                            .collect(Collectors.joining()), 2);
-        return result;
-    }   
 }
-    
-    
-
