8a1a5ecf4ed12dd57547b16b298a5162753c5c4e
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2025-10-23 16:20:10 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,12 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int n = brown + yellow;
-        for(int i = 1;i < n/2; i++){
-            if(n % i == 0){
-                int a = i;
-                int b = n / i;
-                if(brown == 2 * a + 2 * (b-2)) {
-                    answer[0] = a;
-                    answer[1] = b;
-                }
-            }
+        int total = brown + yellow;
+
+        int a = brown / 2 + 2;
+        for(int i = a; i >= 3; i--){
+            int j = a - i;
+            if(j > 0 && i >= j && i * j == total) answer = new int[] {i,j};
         }
         return answer;
     }
