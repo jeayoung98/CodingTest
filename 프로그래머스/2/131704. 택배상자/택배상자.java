f3258bdad59b8d73a95b0214547a6ccc01c5709c
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2025-10-28 21:28:46 +0900

    [level 2] Title: 택배상자, Time: 54.26 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 class Solution {
+    int answer, index = 0;
     public int solution(int[] order) {
+        
         Stack<Integer> stack = new Stack<>();
-        int index = 0; 
 
         for (int i = 1; i <= order.length; i++) {
-            while (!stack.isEmpty() && stack.peek() == order[index]) {
-                stack.pop(); 
-                index++; 
-            }
-
-            if (i == order[index]) {
-                index++; 
+            putToTruck(stack, order);
+            if (order[index] == i) {
+                answer++; 
+                index++;
             } else {
                 stack.push(i);
             }
         }
+        putToTruck(stack, order);
 
+        return answer;
+    }
+    
+    public void putToTruck(Stack<Integer> stack, int[] order){
         while (!stack.isEmpty() && stack.peek() == order[index]) {
-            stack.pop(); 
-            index++; 
+            stack.pop();
+            answer++;
+            index++;
         }
-
-        return index--; 
     }
 }
