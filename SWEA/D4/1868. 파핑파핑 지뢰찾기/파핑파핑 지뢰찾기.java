4cb227f493f141d1571f03ea9bcd3252d0dfedb0
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-01-02 15:11:31 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 196 ms, Memory: 45,140 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -11,32 +11,37 @@ public class Solution{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= T; i++) {
             sb.append("#").append(i).append(" ");
             int N = Integer.parseInt(br.readLine());
-            String[][] arr = new String[N][N];
+            
+            char[][] arr = new char[N][N];
             boolean[][] visited = new boolean[N][N];
+
             for (int j = 0; j < N; j++) {
-                arr[j] = br.readLine().split("");
+                arr[j] = br.readLine().toCharArray();
             }
 
             int[][] mineArr = new int[N][N];
             for (int x = 0; x < N; x++) {
                 for (int y = 0; y < N; y++) {
-                    if(arr[x][y].equals(".")) mineArr[x][y] = countMine(arr,x,y);
+                    if(arr[x][y] == '.') mineArr[x][y] = countMine(arr,x,y);
                     else mineArr[x][y] = -1;
                 }
             }
 
             int result = 0;
+            
             for (int x = 0; x < N; x++) {
                 for (int y = 0; y < N; y++) {
                     if(!visited[x][y] && mineArr[x][y] == 0) {
-                        if(bfs(mineArr, visited,x,y)) result++;
+                        bfs(mineArr, visited, x, y);
+                        result++;
                     }
                 }
             }
-
+            
             for (int q = 0; q < N; q++) {
                 for (int p = 0; p < N; p++) {
                     if (!visited[q][p] && mineArr[q][p] != -1) {
@@ -45,49 +50,58 @@ public class Solution{
                     }
                 }
             }
-            sb.append(result).append("\n");
 
+            sb.append(result).append("\n");
         }
+
         System.out.println(sb);
     }
 
-    public static int countMine(String[][] arr, int x, int y) {
+    public static int countMine(char[][] arr, int x, int y) {
         int count = 0;
         int len = arr.length;
+
         for (int i = 0; i < dx.length; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx >= 0 && nx < len && ny >= 0 && ny < len && arr[nx][ny].equals("*")) count++;
-        }
 
+            if(nx >= 0 && nx < len && ny >= 0 && ny < len &&
+                    arr[nx][ny] == '*') {
+                count++;
+            }
+        }
         return count;
     }
-
-    public static boolean bfs(int[][] mineArr,boolean[][] visited, int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        int count = 0;
+    
+    public static boolean bfs(int[][] mineArr, boolean[][] visited, int x, int y) {
         int len = mineArr.length;
+        Queue<int[]> queue = new ArrayDeque<>();
+
         queue.add(new int[]{x, y});
         visited[x][y] = true;
+
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
+            int cx = current[0];
+            int cy = current[1];
+
+            if (mineArr[cx][cy] != 0) continue;
+
             for (int i = 0; i < dx.length; i++) {
-                int cx = current[0];
-                int cy = current[1];
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if (nx >= 0 && nx < len && ny >= 0 && ny < len && !visited[nx][ny] && mineArr[nx][ny] != -1) {
-                    visited[nx][ny] = true;
-                    if (mineArr[nx][ny] == 0){
-                        queue.add(new int[]{nx, ny});
-                    }
-                    count++;
+                if (nx < 0 || nx >= len || ny < 0 || ny >= len) continue;
+                if (visited[nx][ny]) continue;
+                if (mineArr[nx][ny] == -1) continue;
+
+                visited[nx][ny] = true;
+
+                if (mineArr[nx][ny] == 0) {
+                    queue.add(new int[]{nx, ny});
                 }
             }
-
         }
-
-        return count >= 1;
+        return true;
     }
 }
