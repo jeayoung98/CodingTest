d5a7930bd5223b1043f37471b318e6e64d0eb8fd
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-01-02 13:49:18 +0900

    [D3] Title: 항구에 들어오는 배, Time: 574 ms, Memory: 59,760 KB -BaekjoonHub

---
+++ SWEA/D3/4371. 항구에 들어오는 배/README.md
@@ -0,0 +1,15 @@
+# [D3] 항구에 들어오는 배 - 4371 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWMedCxalW8DFAXd) 
+
+### 성능 요약
+
+메모리: 59,760 KB, 시간: 574 ms, 코드길이: 1,460 Bytes
+
+### 제출 일자
+
+2026-01-02 13:49
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4371. 항구에 들어오는 배/항구에 들어오는 배.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		        Scanner sc = new Scanner(System.in);
+        long T = sc.nextLong();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= T; i++) {
+            sb.append("#").append(i).append(" ");
+            int N = sc.nextInt();
+            long[] arr = new long[N - 1];
+            boolean[] visited = new boolean[N - 1];
+            int boat = 0;
+            sc.nextInt();
+            for (int j = 0; j < N-1; j++) {
+                arr[j] = sc.nextLong() - 1L;
+            }
+            boolean flag = true;
+            while (flag) {
+                boolean currentFlag = false;
+                for (int k = 0; k < arr.length; k++) {
+                    if (!visited[k]) {
+                        long currentNum = arr[k];
+                        visited[k] = true;
+                        currentFlag = true;
+                        for (int q = k + 1; q < arr.length; q++) {
+                            if (!visited[q] && arr[q] % currentNum == 0) {
+                                visited[q] = true;
+                            }
+                        }
+
+                        boat++;
+                        break;
+                    }
+
+                }
+                flag = currentFlag;
+            }
+            sb.append(boat).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
