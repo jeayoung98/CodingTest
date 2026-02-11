096870b1618324f23a6460f293869a2c1e993c73
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-02-11 15:32:35 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -3,44 +3,44 @@ import java.util.*;
 
 public class Main {
     static int N, M;
+    static int[] nums;
     static int[] arr;
-    static int[] result;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        String[] s1 = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-        arr = new int[N];
-        result = new int[M];
-        visited = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(s1[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N + 1];
+        arr = new int[M];
+        visited = new boolean[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        dfs(0, 0);
-        System.out.println(sb.toString());
+        Arrays.sort(nums);
+        dfs(1, 0);
+        System.out.println(sb);
     }
 
     public static void dfs(int start, int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(result[i]).append(" ");
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         int last = -1;
-        for (int i = start; i < N; i++) {
-            if (last == arr[i]) continue;
+        for (int i = start; i <= N; i++) {
+            if (last == nums[i]) continue;
             if (!visited[i]) {
                 visited[i] = true;
-                result[depth] = arr[i];
-                last = arr[i];
+                arr[depth] = nums[i];
+                last = nums[i];
                 dfs(i + 1, depth + 1);
                 visited[i] = false;
             }
