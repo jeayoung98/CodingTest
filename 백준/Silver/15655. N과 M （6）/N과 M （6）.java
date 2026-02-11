0412e809fd86d66bce646519e3bc390800c00bc8
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-02-11 14:40:31 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,40 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static int[] result;
+    static int N, M, lastIdx;
     static boolean[] visited;
+    static int[] nums;
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-        result = new int[M];
-        visited = new boolean[N];
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(arr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        nums = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
         dfs(0);
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    public static void dfs(int depth) {
+    private static void dfs(int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(result[i]).append(" ");
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = depth == 0 ? 0 : Arrays.binarySearch(arr,result[depth - 1]) + 1; i < N; i++) {
+        for (int i = depth == 0 ? 1: Arrays.binarySearch(nums,arr[depth - 1]) + 1; i <= N; i++) {
             if (!visited[i]) {
-                result[depth] = arr[i];
                 visited[i] = true;
+                arr[depth] = nums[i];
                 dfs(depth + 1);
                 visited[i] = false;
             }
