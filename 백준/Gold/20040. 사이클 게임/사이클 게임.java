927050ab1b1db80703aadc67d570d9e69dd2bded
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-03-11 17:53:56 +0900

    [Gold IV] Title: 사이클 게임, Time: 572 ms, Memory: 120420 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int[] parent;
-
+    static int[] size;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -13,9 +13,11 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         parent = new int[N];
+        size = new int[N];
 
         for (int i = 0; i < N; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
 
 
@@ -40,12 +42,20 @@ public class Main {
         return parent[x] = find(parent[x]);
     }
 
-    public static boolean union(int a, int b) {
+    public static boolean union(int a, int b){
+
         int pa = find(a);
         int pb = find(b);
 
-        if (pa == pb) return true;
-        parent[pb] = pa;
+        if(pa == pb) return true;
+
+        if(size[pa] < size[pb]){
+            parent[pa] = pb;
+            size[pb] += size[pa];
+        }else{
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        }
         return false;
     }
 }
