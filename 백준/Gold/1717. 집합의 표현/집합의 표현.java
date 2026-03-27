7981235c1af3960374d26e66e46b4c11f4b598bc
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-03-28 00:17:58 +0900

    [Gold V] Title: 집합의 표현, Time: 412 ms, Memory: 51028 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,43 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int[] parent;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         parent = new int[N + 1];
-
         for (int i = 0; i <= N; i++) {
             parent[i] = i;
         }
 
-        
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-
             st = new StringTokenizer(br.readLine());
-
-            int cmd = Integer.parseInt(st.nextToken());
+            int command = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (cmd == 0) {
+            if (command == 0) {
                 union(a, b);
             } else {
-                if (find(a) == find(b)) sb.append("YES\n");
-                else sb.append("NO\n");
+                sb.append(find(a) == find(b) ? "YES" : "NO").append("\n");
             }
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
+
     }
 
     public static int find(int x) {
