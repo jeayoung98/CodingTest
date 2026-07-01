61bf85153643e1e9384a00cd1e4f93c0c16a9487
Jeayoung <160308043+jeayoung98@users.noreply.github.com>
2026-07-01 21:44:40 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.18 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        int time = 0;
+        int idx = 0;
+        int total = 0;
+        int count = 0;
+
+        while (count < jobs.length) {
+            while (idx < jobs.length && jobs[idx][0] <= time) {
+                pq.add(jobs[idx]);
+                idx++;
+            }
+
+            if (!pq.isEmpty()) {
+                int[] job = pq.poll();
+
+                time += job[1];
+                total += time - job[0];
+                count++;
+            } else {
+                time = jobs[idx][0];
+            }
+        }
+
+        return total / jobs.length;
+    }
+}
